a704c3cf268d90da7c8aabb836378952c898578f
Gusev Daniil <guseff.daniil2011@yandex.ru>
2022-01-10 11:09:04 +0300

    edit cors

--- skycards/settings.py
+++ skycards/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
                       'content-type', 'accept', 'origin', 'authorization')
