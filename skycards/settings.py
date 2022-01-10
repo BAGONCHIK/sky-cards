3f549a80ca34480080be5d05f9061279d96252df
Gusev Daniil <guseff.daniil2011@yandex.ru>
2022-01-10 11:03:51 +0300

    edit cors

--- skycards/settings.py
+++ skycards/settings.py
@@ -29,13 +29,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    "http://localhost:3000",
-    'https://still-mountain-84233.herokuapp.com',
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
