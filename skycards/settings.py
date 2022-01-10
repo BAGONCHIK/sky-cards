91ed4552868c004c3aafaecdd54ce8ca0b7e3258
Gusev Daniil <guseff.daniil2011@yandex.ru>
2022-01-10 11:10:55 +0300

    edit cors

--- skycards/settings.py
+++ skycards/settings.py
@@ -31,9 +31,27 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
-                      'content-type', 'accept', 'origin', 'authorization')
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
 
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
 
 # Application definition
 
